da4aeb3902d1d5b0a85b4f7b789438ea2b797b57
camelbg <tihomir.ivanov1564@gmail.com>
2018-12-23 22:48:41 +0200

    Implement Crop and KeepAspect image resizer.

--- ImageAPI/Resizer/Strategies/CropStrategy.cs
+++ ImageAPI/Resizer/Strategies/CropStrategy.cs
@@ -1,4 +1,5 @@
 ﻿using ImageAPI.Interfaces;
+using System.Drawing;
 
 namespace ImageAPI.Resizer.Strategies
 {
@@ -6,7 +7,17 @@ namespace ImageAPI.Resizer.Strategies
     {
         public void Process(string sourceFile, string destinationFile, int width, int height, int startX, int startY)
         {
-            throw new System.NotImplementedException();
+            var point = new Point(startX, startY);
+            var size = new Size(width, height);
+            var srcRect = new Rectangle(point, size);
+            var sourceImage = Image.FromFile(sourceFile) as Bitmap;
+            var destinationImage = new Bitmap(srcRect.Width, srcRect.Height);
+
+            using (var graphics = Graphics.FromImage(destinationImage))
+            {
+                var destRect = new Rectangle(0, 0, destinationImage.Width, destinationImage.Height);
+                graphics.DrawImage(sourceImage, destRect, srcRect, GraphicsUnit.Pixel);
+            }
         }
     }
 }
--- ImageAPI/Resizer/Strategies/KeepAspectStrategy.cs
+++ ImageAPI/Resizer/Strategies/KeepAspectStrategy.cs
@@ -1,4 +1,7 @@
 ﻿using ImageAPI.Interfaces;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace ImageAPI.Resizer.Strategies
 {
@@ -6,7 +9,51 @@ namespace ImageAPI.Resizer.Strategies
     {
         public void Process(string sourceFile, string destinationFile, int width, int height, int startX, int startY)
         {
-            throw new System.NotImplementedException();
+            var imgPhoto = Image.FromFile(sourceFile);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+            int destX = 0;
+            int destY = 0;
+
+            float nPercent = 0;
+            float nPercentW = 0;
+            float nPercentH = 0;
+
+            nPercentW = ((float)width / (float)sourceWidth);
+            nPercentH = ((float)height / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((width - (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((height - (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.Red);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+
+            bmPhoto.Save(destinationFile);
         }
     }
 }
