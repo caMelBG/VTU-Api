6540e904c3e34182ac3e6b8477ef6544922b2637
tivanov <melonsysops@outlook.com>
2019-01-17 11:28:51 +0200

    add resize functionality to web api project

--- WebClient/Controllers/WebApiController.cs
+++ WebClient/Controllers/WebApiController.cs
@@ -3,6 +3,8 @@ using ImageAPI.Enums;
 using System;
 using System.Web.Http;
 using System.Web.Hosting;
+using ImageAPI.Resizer;
+using ImageAPI.Resizer.Strategies;
 
 namespace WebClient.Controllers
 {
@@ -32,6 +34,37 @@ namespace WebClient.Controllers
             {
                 return BadRequest(ex.Message);
             }
+        }
+
+        [HttpGet]
+        public IHttpActionResult Resize(string sourceImagePath, string destinationImageName, ResizeType resizeType, int width, int height, int x, int y)
+        {
+            try
+            {
+                var lastIndex = sourceImagePath.LastIndexOf('/');
+                var outputImageDirectory = sourceImagePath.Substring(0, lastIndex);
+                var imageName = sourceImagePath.Substring(lastIndex + 1, sourceImagePath.Length - lastIndex - 1);
+                var extension = this.GetFileExtension(imageName);
+                var srcImage = HostingEnvironment.MapPath(sourceImagePath);
+                var destImage = HostingEnvironment.MapPath(outputImageDirectory + "/" + destinationImageName + "." + extension);
+                var imagePath = outputImageDirectory + "/" + destinationImageName + "." + extension;
+
+                var resizer = new Resizer(new ResizeStrategyCreator());
+                resizer.Resize(srcImage, destImage, resizeType, width, height, x, y);
+
+                return Ok(imagePath);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        private string GetFileExtension(string fileName)
+        {
+            var indexOf = fileName.LastIndexOf('.');
+            var extension = fileName.Substring(indexOf + 1, fileName.Length - indexOf - 1);
+            return extension;
         }
     }
 }
